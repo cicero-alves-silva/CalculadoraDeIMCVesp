776a4a3ea151375b247648e8f63a17a91dbd49c5
CICERO <cicero.alves@ifpb.edu.br>
2025-08-14 10:07:11 -0300

    Aula 13/08/2025

--- app/src/main/java/com/example/calculadoradeimcvesp/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradeimcvesp/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.calculadoradeimcvesp
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.calculadoradeimcvesp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btOk.setOnClickListener { view ->
+            var categoria = ""
+            when(binding.rgCategorias.checkedRadioButtonId){
+                binding.rbAdultos.id -> categoria = "ADULTO"
+                binding.rbIdosos.id -> categoria = "IDOSO"
+                else ->
+                    Snackbar
+                        .make(
+                            view,
+                            "Selecione uma as opções acima antes de clicar",
+                            Snackbar.LENGTH_LONG
+                        )
+                        .setBackgroundTint(Color.RED)
+                        .setTextColor(Color.WHITE)
+                        .show()
+            }
+            if (categoria.isNotEmpty()) {
+                //ABRIR A TELA DE CÁLCULO
+            }
         }
     }
 }
