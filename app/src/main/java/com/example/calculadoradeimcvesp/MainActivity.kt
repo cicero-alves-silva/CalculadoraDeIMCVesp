f8b17ac36ac36b25d494cbbbb3ba2cb3acfb79ee
Cicero <cicero.alves@ifpb.edu.br>
2026-08-19 17:38:19 -0300

    Aula 19/08/2026

--- app/src/main/java/com/example/calculadoradeimcvesp/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradeimcvesp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculadoradeimcvesp
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -29,6 +30,9 @@ class MainActivity : AppCompatActivity() {
             }
             if (categoria.isNotEmpty()) {
                 //ABRIR A TELA DE CÁLCULO
+                val intent = Intent(this, CalculoResultadoActivity::class.java)
+                intent.putExtra("CATEGORIA", categoria)
+                startActivity(intent)
             }
         }
 
