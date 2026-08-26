4583871484fcaf9b082d2fe6bae43d780c3423c8
Cicero <cicero.alves@ifpb.edu.br>
2026-08-26 17:59:12 -0300

    Aula 26/08/2026

--- app/src/main/java/com/example/calculadoradeimcvesp/CalculoResultadoActivity.kt
+++ app/src/main/java/com/example/calculadoradeimcvesp/CalculoResultadoActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculadoradeimcvesp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculadoradeimcvesp.databinding.ActivityCalculoResultadoBinding
+import kotlin.math.pow
 
 class CalculoResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +26,42 @@ class CalculoResultadoActivity : AppCompatActivity() {
                 binding.tilAltura.error = "Digite uma altura válida"
                 erroDeValidacao = true
             } else binding.tilAltura.error = ""
+            if (!erroDeValidacao) {
+                val bundle = intent.extras
+                if (bundle != null && peso != null && altura != null){
+                    val categoria = bundle.getString("CATEGORIA")
+                    val imc = peso / altura.pow(2)
+                    val imcArredondado = "%.2f".format(imc).toDouble()
+                    binding.tvResultado.text = when(categoria) {
+                        "ADULTO" -> {
+                            if (imcArredondado < 18.5) "BAIXO PESO"
+                            else if (imcArredondado < 25.0) "PESO NORMAL"
+                            else if (imcArredondado < 30.0) "SOBREPESO"
+                            else if (imcArredondado < 35.0) "OBESIDADE CLASSE I"
+                            else if (imcArredondado < 40.0) "OBESIDADE CLASSE II"
+                            else "OBESIDADE CLASSE III"
+                        }
+
+                        "IDOSO" -> {
+                            if (imcArredondado < 22.0) "BAIXO PESO"
+                            else if (imcArredondado < 27.0) "PESO NORMAL"
+                            else "SOBREPESO"
+                        }
+
+                        else -> "Categoria Inválida!"
+                    }
+                }
+            }
+        }
+
+        binding.includeAppBar.materialToolbar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.miDireitosAutorais -> {
+                    //ABRIR A TELA DOS CRÉDITOS DA APP
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
